a9dc3338250c6a68db70f5343b393fb9fcd0932f
Samuel Albi Pulo Sibarani <samuelalbi09@gmail.com>
2025-06-12 21:35:50 +0700

    fixed setup

--- main.py
+++ main.py
@@ -131,4 +131,6 @@ app.include_router(router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
